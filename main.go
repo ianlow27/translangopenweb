3d93104a4ca7cbdcd3dd8549fe15b8fb0029cc62
Ian Low <ianlow28@gmail.com>
2024-03-27 22:59:46 +0000

    Changes in main.go test1.php 2024-03-72 22:55

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 import "fmt"
 import "github.com/ianlow27/translangopenweb/pkg1"
+import (
+"io/ioutil"
+"net/http"
+)
 func main(){
   fmt.Println("This is a new project called 'translangopenweb'")
   pkg1.Pkg1run()
+  //-------------------------------
+  req, err := http.NewRequest(http.MethodGet,
+    "http://localhost:8000", nil)
+  if(err == nil){
+    var response *http.Response
+    response, err = http.DefaultClient.Do(req)
+    if(err == nil){
+      resp, _ := ioutil.ReadAll(response.Body)
+      fmt.Println(string(resp))
+    }else {
+    }
+  }
+  //-------------------------------
 }
---
+++ test1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Helo Byd</h1>
+<?php
+echo date("Y-m-d H:i:s");
+?>
+</body>
+</html>
