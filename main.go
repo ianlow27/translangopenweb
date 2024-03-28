44add1eacc4a164957a7c936b067d58fe1c5d9c1
Ian Low <ianlow28@gmail.com>
2024-03-28 20:39:37 +0000

    Changes in main.go 2024-03-28 20:39

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
-import "fmt"
-import "github.com/ianlow27/translangopenweb/pkg1"
 import (
-"io/ioutil"
-"net/http"
+//"github.com/ianlow27/translangopenweb/pkg1"
+//"io/ioutil"
+//"net/http"
+"os"
+"bufio"
+"fmt"
+"strings"
 )
 func main(){
-  fmt.Println("This is a new project called 'translangopenweb'")
-  pkg1.Pkg1run()
+  strvocab := 
+`field|maes{f}
+flower|blodeuyn{m}
+fence|clawdd{m}`
+var pr = fmt.Println
+  arr1 := strings.Split(strvocab, "\n");
+  gvocab := map[string]string {}
+  for _, value := range arr1 {
+    //pr(value)
+    lstr1 := strings.Split(value, "|");
+    //for _, value2 := range lstr1 {
+      //pr(value2)
+      gvocab[strings.TrimSpace(lstr1[0])] = strings.TrimSpace(lstr1[1])
+      pr(lstr1[0] + "__" + gvocab[lstr1[0]])
+    //}
+  }
+  reader := bufio.NewReader(os.Stdin)
+  pr("Please enter some text:")
+  text, _ := reader.ReadString('\n')
+  //pr(text)
+  arr2 := strings.Split(strings.TrimSpace(text), " ");
+  text = ""
+  for _, value := range arr2 {
+    text +=  " " + gvocab[value]
+  }
+  pr("Welsh>" + text)
+  /*
+  gvocab := map[string]string {
+    "field": "maes{f}",
+    "flower": "blodeuyn{m}",
+  }
+  */
+  //pr(gvocab["field"])
+  //fmt.Println("This is a new project called 'translangopenweb'")
+  //pkg1.Pkg1run()
   //-------------------------------
+  /*
   req, err := http.NewRequest(http.MethodGet,
     "http://localhost:8000", nil)
   if(err == nil){
@@ -20,5 +57,6 @@ func main(){
     }else {
     }
   }
+  */
   //-------------------------------
 }
